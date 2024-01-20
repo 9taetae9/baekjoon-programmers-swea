1d797dc6ee294a1c33302502609965b0d8b3936e
TaeHyoun Kim <89383263+9taetae9@users.noreply.github.com>
2024-01-20 20:59:45 +0900

    [Platinum V] Title: 버블 소트, Time: 584 ms, Memory: 126224 KB -BaekjoonHub

---
+++ 백준/Platinum/1517. 버블 소트/버블 소트.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.nio.Buffer;
+import java.util.*;
+
+public class Main{
+
+    public static int N;
+    public static int[] arr;
+    public static long ans = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        mergeSort(0, arr.length - 1);
+
+        System.out.println(ans);
+        br.close();
+    }
+
+    public static void mergeSort(int left, int right){
+        if(left < right){
+            int mid = (left + right) >> 1;
+            mergeSort(left, mid);
+            mergeSort(mid+1, right);
+            merge(left, mid, right);
+        }
+    }
+
+    public static void merge(int left, int mid, int right){
+        int[] leftArr = Arrays.copyOfRange(arr, left, mid+1);
+        int[] rightArr = Arrays.copyOfRange(arr, mid + 1, right+1);
+        int idx = left, leftIdx = 0, rightIdx = 0, cnt = leftArr.length;
+
+        while(leftIdx < leftArr.length && rightIdx < rightArr.length){
+            if(leftArr[leftIdx] > rightArr[rightIdx]){
+                arr[idx] = rightArr[rightIdx++];
+                ans += cnt;
+            } else{
+                arr[idx] = leftArr[leftIdx++];
+                cnt--;
+            }
+            idx++;
+        }
+
+        while(rightIdx < rightArr.length){
+            arr[idx++] = rightArr[rightIdx++];
+        }
+
+        while(leftIdx < leftArr.length){
+            arr[idx++] = leftArr[leftIdx++];
+        }
+    }
+}
