39dd069607622eff0b84bf373a712754290153f0
TaeHyoun Kim <89383263+9taetae9@users.noreply.github.com>
2024-01-20 22:13:14 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 224 ms, Memory: 17772 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] dp = new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+
+        if(n>=2) {
+            for (int i = 2; i <= n; i++) {
+                dp[i] = (dp[i - 1] + dp[i - 2] * 2)%10007;
+            }
+        }
+
+        System.out.println(dp[n]%10007);
+
+
+    }
+}
