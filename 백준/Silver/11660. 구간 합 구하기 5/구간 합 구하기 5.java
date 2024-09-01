3eeb9c85baa425457f9ee1bbf8510768e02da012
TaeHyoun Kim <89383263+9taetae9@users.noreply.github.com>
2024-09-01 15:37:26 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 752 ms, Memory: 121852 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,39 +1,41 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
-public class Main{
-
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-
-        int[][] arr = new int[N][N];
-        int[][] presum = new int[N+1][N+1];
-
-        for(int i=0; i<N; i++){
+        
+        int[][] arr = new int[N+1][N+1];
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=0; j<N; j++){
+            for (int j = 1; j <= N; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
-                presum[i+1][j+1] = arr[i][j] + presum[i][j+1] + presum[i+1][j] - presum[i][j];
             }
         }
-
-
-        for(int i=0; i < M; i++){
+        
+        int[][] prefixSum = new int[N+1][N+1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                prefixSum[i][j] = prefixSum[i-1][j] + prefixSum[i][j-1] - prefixSum[i-1][j-1] + arr[i][j];
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
-            System.out.println(getSum(presum, x1, y1, x2, y2));
+            
+            int result = prefixSum[x2][y2] - prefixSum[x1-1][y2] - prefixSum[x2][y1-1] + prefixSum[x1-1][y1-1];
+            sb.append(result).append("\n");
         }
-    }
-
-    private static int getSum(int[][] presum, int x1, int y1, int x2, int y2){
-        return presum[x2][y2] - presum[x2][y1-1] - presum[x1-1][y2] + presum[x1-1][y1-1];
+        
+        System.out.print(sb);
     }
 }
