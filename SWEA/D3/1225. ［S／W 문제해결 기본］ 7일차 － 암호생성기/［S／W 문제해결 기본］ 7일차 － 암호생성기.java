f25fe39eedf2fa32405a4ea15509119991d5e9be
TaeHyoun Kim <89383263+9taetae9@users.noreply.github.com>
2024-09-11 10:36:02 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 120 ms, Memory: 18,424 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -1,31 +1,52 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 class Solution {
-    public static void main(String args[]) throws Exception {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T;
-        T = 10;
+        StringBuilder sb = new StringBuilder();
+        int T = 10; // 10개의 테스트 케이스
+
         for (int test_case = 1; test_case <= T; test_case++) {
-            int t = Integer.parseInt( br.readLine());
-            List<Integer> list = new LinkedList<>();
+            br.readLine(); // 테스트 케이스 번호 (사용하지 않음)
             StringTokenizer st = new StringTokenizer(br.readLine());
+            int[] numbers = new int[8];
+            
+            // 8개의 숫자 입력 받기
             for (int i = 0; i < 8; i++) {
-                list.add(Integer.parseInt(st.nextToken()));
+                numbers[i] = Integer.parseInt(st.nextToken());
             }
-            
-            int num = 0;
-            while (list.get(7) > 0) {
-                int value = list.remove(0) - (num % 5 + 1);
-                list.add(value > 0 ? value : 0);
-                num++;
+
+            boolean finished = false;
+
+            while (!finished) {
+                for (int i = 1; i <= 5; i++) {
+                    int temp = numbers[0] - i;
+                    if (temp <= 0) {
+                        temp = 0;
+                        finished = true;
+                    }
+                    
+                    // 배열 요소 이동
+                    System.arraycopy(numbers, 1, numbers, 0, 7);
+                    numbers[7] = temp;
+
+                    if (finished) break;
+                }
             }
-            
-            System.out.print("#" + t + " ");
-            for (int i : list) {
-                System.out.print(i + " ");
+
+            // 결과 저장
+            sb.append("#").append(test_case);
+            for (int num : numbers) {
+                sb.append(" ").append(num);
             }
-            System.out.println();
+            sb.append("\n");
         }
+        
+        // 전체 결과 한 번에 출력
+        System.out.print(sb);
+        br.close();
     }
 }
