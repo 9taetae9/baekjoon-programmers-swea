545c080c4912666b64a99f3cb8531eef2dc5c7ff
TaeHyoun Kim <89383263+9taetae9@users.noreply.github.com>
2024-11-02 23:58:16 +0900

    [Gold IV] Title: 키 순서, Time: 532 ms, Memory: 36792 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.java
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -6,20 +6,18 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        //첫째 줄에 학생들의 수 N (2 ≤ N ≤ 500)과 두 학생 키를 비교한 횟수 M (0 ≤ M ≤ N(N-1)/2)
+        
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[][] H = new int[N][N];
+        boolean[][] H = new boolean[N][N];
 
         for(int i=0; i < M; i++){
             st = new StringTokenizer(br.readLine());
             int u1 = Integer.parseInt(st.nextToken());
             int u2 = Integer.parseInt(st.nextToken());
 
-            H[u1-1][u2-1] = 1;  //자신보다 큰 사람
-            H[u2-1][u1-1] = -1; //자신보다 작은 사람
+            H[u1-1][u2-1] = true;
         }
 
 
@@ -27,34 +25,24 @@ public class Main {
             for (int i = 0; i < H.length; i++) {
                 if(i == k) continue;
                 for (int j = 0; j < H.length; j++) {
-                    if(H[i][k] == 1 && H[k][j] == 1){ // j가 i보다 크고, k가 j보다 크면 => k는 i보다 큼
-                        H[i][j] = 1;
-                    } else if(H[i][k] == -1 && H[k][j] == -1){ // j가 i보다 작고, k가 j보다 작으면 => k는 i보다 작음
-                        H[i][j] = -1;
+                    if(H[i][k] && H[k][j]) {
+                        H[i][j] = true;
                     }
                 }
             }
         }
 
-        //자신보다 큰 사람은 1, 작은사람은 -1로 저장되어있음
-        //0인 경우 => 자기 자신 or 키 비교를 못하는 경우
-
-        //자기 자신에 해당 해당하는 열 제외하곤 -1 또는 1이 체크 되어 있어야 자신의 순서를 정확히 알 수 있음
         int cnt=0;
         boolean know;
         for(int i=0; i < H.length; i++){
             know = true;
             for(int j=0; j < H.length; j++){
-                if(i==j) continue; // 자기 자신
-                if(H[i][j] == 0) {  // 비교 못하는 경우
+                if(i!=j && !H[i][j] && !H[j][i]){
                     know = false;
-                    break;
                 }
             }
-
             if(know) cnt++;
         }
-
 
         System.out.println(cnt);
     }
