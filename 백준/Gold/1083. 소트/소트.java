22bbcd8033bc5e210edba359cfc1dbe88afa0875
TaeHyoun Kim <89383263+9taetae9@users.noreply.github.com>
2024-11-17 09:42:57 +0900

    [Gold IV] Title: 소트, Time: 120 ms, Memory: 15856 KB -BaekjoonHub

--- 백준/Gold/1083. 소트/소트.java
+++ 백준/Gold/1083. 소트/소트.java
@@ -1,37 +1,41 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-	static List<Integer> arr;
+    static List<Integer> list;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(bufferedReader.readLine());
-		
-		arr = new ArrayList<>();
-		
-		StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine()); 
-		for(int i=0; i<N; i++) {
-			arr.add(Integer.parseInt(stringTokenizer.nextToken()));
-		}
-		
-		int S = Integer.parseInt(bufferedReader.readLine());
-		
-		for(int i=0; i < N; i++) {
-			int maxIndex = i; 
-			for(int j=i+1; j<N &&j-i<=S; j++ ) {
-				if(arr.get(maxIndex)<arr.get(j)) maxIndex = j;
-			}
-			
-			for(int j=maxIndex; j>i; j--) {
-				Collections.swap(arr, j, j-1);
-			}
-			
-			S -= (maxIndex - i);
-		}
-		
-		for(int e : arr) {
-			System.out.print(e+" ");
-		}	
-	}
+        list = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int S = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<N; i++){
+            int maxIndex = i;
+            for(int j=i+1; j<N && j-i<=S; j++){
+                if(list.get(maxIndex) < list.get(j)){
+                    maxIndex = j;
+                }
+            }
+
+            for(int j=maxIndex; j>i; j--){
+                Collections.swap(list, j, j-1);
+            }
+
+            S -= (maxIndex - i);
+        }
+
+        for(int e : list){
+            System.out.print(e+" ");
+        }
+    }
 }
