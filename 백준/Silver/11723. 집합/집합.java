0054d79cbb6f847bce17071ac9ef19b7f732f4bd
TaeHyoun Kim <kth9903@gmail.com>
2025-01-21 09:49:51 +0900

    [Silver V] Title: 집합, Time: 1164 ms, Memory: 308500 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,45 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
-    static boolean[] S = new boolean[21];
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        BitSet S = new BitSet(21);
 
         int M = Integer.parseInt(br.readLine());
 
-        for(int i=0; i < M; i++){
+        for (int i = 0; i < M; i++) {
             String[] query = br.readLine().split(" ");
-            int n = 0;
-            if(query.length == 2){
-                n = Integer.parseInt(query[1]);
+
+            switch (query[0]) {
+                case "all" -> {
+                    S.set(1, 21); // 1 ~ 20 까지 모든 비트 1로 설정
+
+                    continue; // 1 ~ 20 까지 모든 비트 1로 설정
+                }
+                case "empty" -> {
+                    S.clear();
+                    continue;
+                }
             }
+
+            int n = Integer.parseInt(query[1]);
             switch (query[0]){
-                case "add" :
-                    S[n] = true;
-                    break;
-                case "remove" :
-                    S[n] = false;
-                    break;
-                case "check" :
-                    sb.append(S[n] ? 1 : 0).append('\n');
-                    break;
-                case "toggle" :
-                    S[n] = !S[n];
-                    break;
-                case "all" :
-                    Arrays.fill(S, true);
-                    break;
-                case "empty" :
-                    Arrays.fill(S, false);
-                    break;
+                case "add" -> S.set(n);
+                case "remove" -> S.clear(n);
+                case "check" -> sb.append(S.get(n) ? 1 : 0).append('\n');
+                case "toggle" -> S.flip(n);
             }
         }
-
         System.out.println(sb);
     }
 }
