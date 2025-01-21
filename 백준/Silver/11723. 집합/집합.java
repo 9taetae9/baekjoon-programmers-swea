ce443bbfe4a53706b0c1e5ed1a04bfa5e60b1384
TaeHyoun Kim <kth9903@gmail.com>
2025-01-21 09:20:48 +0900

    [Silver V] Title: 집합, Time: 1196 ms, Memory: 307088 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,55 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.Arrays;
 
 public class Main {
-    static final List<Integer> AllNumbers =
-            IntStream.rangeClosed(1,20)
-                    .boxed()
-                    .collect(Collectors.toList());
+    public static void main(String[] args) throws IOException{
+        boolean[] S = new boolean[21];
 
-    public static void main(String[] args) throws IOException {
-        Set<Integer> S = new HashSet<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
         int M = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < M; i++) {
+        int n = 0;
+        for(int i=0; i < M; i++){
             String[] query = br.readLine().split(" ");
-            if(query[0].equals("all")){
-                S.addAll(AllNumbers);
-                continue;
+            if(query.length == 2){
+                n = Integer.parseInt(query[1]);
             }
-            if(query[0].equals("empty")){
-                S.clear();
-                continue;
-            }
-
-            Integer n = Integer.parseInt(query[1]);
-            
             switch (query[0]){
-                case "add":
-                    S.add(n);
+                case "add" :
+                    S[n] = true;
                     break;
-                case "remove":
-                    S.remove(n);
+                case "remove" :
+                    S[n] = false;
                     break;
                 case "check" :
-                    sb.append(S.contains(n) ? 1 : 0).append('\n');
+                    sb.append(S[n] ? 1 : 0).append('\n');
                     break;
-                case "toggle":
-                    if(S.contains(n)){
-                        S.remove(n);
-                    }else S.add(n);
+                case "toggle" :
+                    S[n] = !S[n];
+                    break;
+                case "all" :
+                    Arrays.fill(S, true);
+                    break;
+                case "empty" :
+                    Arrays.fill(S, false);
                     break;
             }
         }
+
         System.out.println(sb);
     }
 }
