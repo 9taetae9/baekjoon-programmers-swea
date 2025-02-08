4dbd3a6b46d0d1bfa4b7e6aaac3c2af2006b9371
TaeHyoun Kim <kth9903@gmail.com>
2025-02-08 11:09:29 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 184 ms, Memory: 23456 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -1,24 +1,45 @@
 import java.io.*;
 
 public class Main {
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedWriter bw;
+    static char[] buffer;
+    static int idx;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        System.out.println((1<<N)-1);
-        move(1, 3, N);
+        int N = Integer.parseInt(br.readLine());
+        // 필요한 버퍼 크기 계산: 각 줄은 최대 4글자 (두 숫자 + 공백 + 개행)
+        // 총 이동 횟수는 2^N - 1
+        int moves = (1 << N) - 1;
+        buffer = new char[4 * moves];
+
+        bw.write(String.valueOf(moves));
+        bw.write('\n');
+
+        hanoi(N, '1', '2', '3');
+
+        bw.write(buffer, 0, idx);
         bw.flush();
+        br.close();
+        bw.close();
     }
 
-    private static void move(int s, int e, int n) throws IOException {
-        if(n == 1) {
-            bw.write(s+" "+e);
-            bw.newLine();
+    private static void hanoi(int n, char from, char aux, char to) throws IOException {
+        if (n == 1) {
+            buffer[idx++] = from;
+            buffer[idx++] = ' ';
+            buffer[idx++] = to;
+            buffer[idx++] = '\n';
             return;
         }
-        move(s,6-(s+e),n-1);
-        move(s, e, 1);
-        move(6-(s+e), e, n-1);
+
+        hanoi(n - 1, from, to, aux);
+        buffer[idx++] = from;
+        buffer[idx++] = ' ';
+        buffer[idx++] = to;
+        buffer[idx++] = '\n';
+        hanoi(n - 1, aux, from, to);
     }
 }
