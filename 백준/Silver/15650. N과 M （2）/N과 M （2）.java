b9da2e1f0f0e87905bbeb2b8886a143f6bd5c672
TaeHyoun Kim <kth9903@gmail.com>
2025-02-09 12:45:03 +0900

    [Silver III] Title: N과 M (2), Time: 132 ms, Memory: 15896 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static boolean[] isUsed = new boolean[9];
+    //    static int cnt;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken()); //1~N까지의 수열
+        M = Integer.parseInt(st.nextToken());  //고를 개수
+        arr = new int[M];
+
+        backtrack(0,0);
+    }
+
+
+    private static void backtrack(int prev, int n) {
+        if (n == M) {
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i]+" ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = prev+1; i <= N; i++) {
+            if (!isUsed[i]) {
+                arr[n] = i;
+                isUsed[i] = true;
+                backtrack(i,n+1);
+                isUsed[i] = false;
+            }
+        }
+    }
+
+}
