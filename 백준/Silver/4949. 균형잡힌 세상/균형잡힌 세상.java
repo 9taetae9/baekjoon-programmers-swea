aeeafd47523ce5a0c3471ed399fb47fcc40b1cab
TaeHyoun Kim <kth9903@gmail.com>
2025-02-26 12:01:28 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 208 ms, Memory: 19848 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -1,40 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        while(true){
-            String line = br.readLine();
-
-            if(line.equals(".")){
-                break;
-            }
-
-            line = line.trim();
-
-            System.out.println(isBalanced(line) ? "yes" : "no");
-        }
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String line = br.readLine();
+    while(!line.equals(".")){
+      System.out.println(isBalanced(line) ? "yes" : "no");
+      line = br.readLine();
     }
+  }
 
-    private static boolean isBalanced(String line){
-        Stack<Character> stack = new Stack<>();
-        for(char c : line.toCharArray()){
-            if(c == '(' || c =='['){
-                stack.push(c);
-            }else if(c == ')'){
-                if(stack.isEmpty() || stack.pop() != '('){
-                    return false;
-                }
-            }else if(c == ']'){
-                if(stack.isEmpty() || stack.pop() != '['){
-                    return false;
-                }
-            }
+  private static boolean isBalanced(String line) {
+    Deque<Character> stack = new ArrayDeque<>();
+    for(char c :  line.toCharArray()){
+      if(c == '(' || c == '['){
+        stack.push(c);
+      }else if(c == ')' || c== ']'){
+        if(stack.isEmpty()) return false;
+        char e = stack.pop();
+        if( e != '(' && c == ')'){
+          return false;
         }
-        return stack.isEmpty();
+        if(e != '[' && c == ']') {
+          return false;
+        }
+      }
     }
+    return stack.isEmpty();
+  }
 }
