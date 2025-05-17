ce609fa9582cbcfaeb3d575cf8b78e95667c9360
TaeHyoun Kim <kth9903@gmail.com>
2025-05-17 13:37:06 +0900

    [level 2] Title: 괄호 회전하기, Time: 33.52 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,41 +1,33 @@
 import java.util.*;
+
 class Solution {
-    char[] word;
-    int len;
     public int solution(String s) {
-        word = s.toCharArray();
-        len = word.length;
         int cnt = 0;
-        for(int i=0; i<len; i++){
-            if(isCorrect(i)) cnt++;
+        for(int i=0; i<s.length(); i++){
+            if(isValid(s, i)) cnt++;
         }
-        
         return cnt;
     }
     
-    private boolean isCorrect(int idx){
-        Deque<Character> stack = new ArrayDeque<>();
+    private boolean isValid(String s, int x){
+        s = s.substring(x) + s.substring(0, x);
         
-        for(int i=0; i<len; i++){
-            char c = word[(idx+i)%len];
-            
-            if(c == '[' || c == '(' || c == '{'){
-                stack.push(c);
-            }else{
-                if(stack.isEmpty()){
-                    return false;
-                }
-                
-                char temp = stack.pop();
-                if(!((c == ']' && temp == '[') ||
-                  (c == '}' && temp == '{') ||
-                  (c == ')' && temp == '('))){
-                    return false;
-                }
-            
+        Deque<Character> stack = new ArrayDeque<>();
+        for(char c : s.toCharArray()){
+            switch(c){
+                case ')':
+                    if(stack.isEmpty() || stack.pop() != '(') return false;
+                    break;
+                case '}':
+                    if(stack.isEmpty() || stack.pop() != '{') return false;
+                    break;
+                case ']':
+                    if(stack.isEmpty() || stack.pop() != '[') return false;
+                    break;
+                default :
+                    stack.push(c);
             }
         }
-        
         return stack.isEmpty();
     }
 }
