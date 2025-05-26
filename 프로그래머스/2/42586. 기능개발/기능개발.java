d7a763833b16ef1370b687d9b3eb7682e71e5237
TaeHyoun Kim <kth9903@gmail.com>
2025-05-26 15:44:27 +0900

    [level 2] Title: 기능개발, Time: 20.88 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,52 +2,29 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        if(progresses.length == 1){
-            return new int[]{1};
-        }
-
-        int len = speeds.length;
+        int len = progresses.length;
         
-        int[] daysNeed = new int[len];
-        
-        for(int i=0; i < len; i++){
-            int remain = 100 - progresses[i];
-            int days = remain / speeds[i];
-            if(remain % speeds[i] != 0) days++;
-            daysNeed[i] = days;
+        int[] leftDays = new int[len];
+        for(int i=0; i<len; i++){
+            leftDays[i] = (int)Math.ceil((100.0 - progresses[i])/speeds[i]);
         }
         
-        for(int i=1; i< len; i++){
-            if(daysNeed[i-1] >= daysNeed[i]){
-                daysNeed[i] = daysNeed[i-1];
-            }
-        }
-        
-        List<Integer> dayList= new ArrayList<>();
-    
-        int prevDay = daysNeed[0];
         int cnt = 1;
+        int maxDays = leftDays[0];
+        
+        List<Integer> ans = new ArrayList<>();
         for(int i=1; i<len; i++){
-            if(prevDay != daysNeed[i]){
-                dayList.add(cnt);
-                prevDay = daysNeed[i];
-                cnt = 1;
-            }else{
+            
+            if(leftDays[i] <= maxDays){
                 cnt++;
+            }else{
+                ans.add(cnt);
+                cnt = 1;
+                maxDays = leftDays[i];
             }
         }
-        dayList.add(cnt);
         
-        
-        int[] answer = new int[dayList.size()];
-        for(int i=0; i < dayList.size(); i++){
-            answer[i] = dayList.get(i);
-        }
-        return answer;
+        ans.add(cnt);
+        return ans.stream().mapToInt(Integer::intValue).toArray();
     }
 }
-
-
-//5 10 1 1 20 1
-//5 10 10 10 20 20
-//1 3 2
