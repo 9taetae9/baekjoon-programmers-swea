74f4a2cf74af8a7eb8734673173bb5ba37ca3aa9
TaeHyoun Kim <kth9903@gmail.com>
2025-05-26 19:25:50 +0900

    [level 2] Title: 할인 행사, Time: 58.48 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,37 +1,25 @@
 import java.util.*;
+
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
-        Map<String, Integer> wishMap = new HashMap<>();
+        Map<String, Integer> wantMap = new HashMap<>();
         for(int i=0; i<want.length; i++){
-            wishMap.put(want[i],number[i]);
+            wantMap.put(want[i], number[i]);
         }
         
-        int count = 0;
-        for(int i=0; i<10; i++){
-            if(wishMap.containsKey(discount[i])){
-            wishMap.put(discount[i], wishMap.get(discount[i])-1);
+        int answer = 0;
+        for(int i=0; i < discount.length - 9; i++){
+            Map<String, Integer> tempMap = new HashMap<>(wantMap);
+            for(int j=i; j < i+10; j++){
+                String key = discount[j];
+                if(!tempMap.containsKey(key)) {
+                    break;
+                }
+                tempMap.computeIfPresent(key, (k, val) -> val > 1 ? val - 1 : null);
             }
+            if(tempMap.size() == 0) answer++;
         }
         
-        if(check(wishMap)) count++;
-        
-        for(int i=10; i < discount.length; i++){
-            if(wishMap.containsKey(discount[i-10])){
-                wishMap.put(discount[i-10], wishMap.get(discount[i-10])+1);
-            }
-            if(wishMap.containsKey(discount[i])){
-                wishMap.put(discount[i], wishMap.get(discount[i])-1);
-            }
-            if(check(wishMap)) count++;
-        }
-        
-        return count;
-    }
-    
-    private boolean check(Map<String, Integer> wishMap){
-        for(int value : wishMap.values()){
-            if(value != 0) return false;
-        }
-        return true;
+        return answer;
     }
 }
