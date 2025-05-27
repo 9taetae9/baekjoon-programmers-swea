4821d1bf4f578a6617b83e045e4d77b1939c438c
TaeHyoun Kim <kth9903@gmail.com>
2025-05-27 18:16:13 +0900

    [level 1] Title: 신고 결과 받기, Time: 227.12 ms, Memory: 158 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -3,38 +3,36 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         Map<String, Integer> userIdx = new HashMap<>();
-        Map<String, Integer> userToReport = new HashMap<>();
-        Set<String> reportSet = new HashSet<>(); //muzi frodo
-        Map<String, List<String>> reportPeople = new HashMap<>();
+        Map<String, Set<String>> reportedUser = new HashMap<>();
+        Map<String, Integer> mailCount = new HashMap<>();
         
         for(int i=0; i<id_list.length; i++){
             userIdx.put(id_list[i], i);
         }
         
         for(String r : report){
-            if(!reportSet.contains(r)){
-                reportSet.add(r);
-                String[] part = r.split(" ");
-                if(!reportPeople.containsKey(part[1])){
-                    reportPeople.put(part[1], new ArrayList<>());
+            String[] part = r.split(" ");
+            if(!reportedUser.containsKey(part[1])){
+                reportedUser.put(part[1], new HashSet<>());
+            }
+            
+            reportedUser.get(part[1]).add(part[0]);
+        }
+        
+        
+        
+        for(Set<String> mailSet: reportedUser.values()){
+            if(mailSet.size() >= k){
+                for(String reporter : mailSet){
+                    mailCount.merge(reporter, 1, Integer::sum);
                 }
-                reportPeople.get(part[1]).add(part[0]);
-                
-                userToReport.merge(part[1], 1, Integer::sum);
             }
         }
         
         int[] answer = new int[id_list.length];
-        for(Map.Entry<String, Integer> userReport : userToReport.entrySet()){
-            if(userReport.getValue() >= k){
-                String key = userReport.getKey(); //frodo : 2 frodo 추출 
-                //신고했던 사람들
-                for(String p : reportPeople.get(key)){
-                    answer[userIdx.get(p)]++;
-                }
-            }
+        for(Map.Entry<String,Integer> m : mailCount.entrySet()){
+            answer[userIdx.get(m.getKey())] = m.getValue();
         }
-        
         return answer;
     }
 }
