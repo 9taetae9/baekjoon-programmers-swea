4a01fb9c44a232115a333195029aaf29fc867ffd
TaeHyoun Kim <kth9903@gmail.com>
2025-05-28 11:18:37 +0900

    [level 1] Title: 신고 결과 받기, Time: 166.53 ms, Memory: 176 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -2,37 +2,35 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        Map<String, Integer> userIdx = new HashMap<>();
         Map<String, Set<String>> reportedUser = new HashMap<>();
-        Map<String, Integer> mailCount = new HashMap<>();
-        
-        for(int i=0; i<id_list.length; i++){
-            userIdx.put(id_list[i], i);
-        }
+        Map<String, Integer> userReports = new HashMap<>();
         
         for(String r : report){
             String[] part = r.split(" ");
-            if(!reportedUser.containsKey(part[1])){
-                reportedUser.put(part[1], new HashSet<>());
+            String reporter = part[0];
+            String reported = part[1];
+            
+            if(!reportedUser.containsKey(reported)){
+                reportedUser.put(reported, new HashSet<>());
             }
             
-            reportedUser.get(part[1]).add(part[0]);
+            reportedUser.get(reported).add(reporter);
         }
         
-        
-        
-        for(Set<String> mailSet: reportedUser.values()){
-            if(mailSet.size() >= k){
-                for(String reporter : mailSet){
-                    mailCount.merge(reporter, 1, Integer::sum);
+        for(Set<String> set : reportedUser.values()){
+            if(set.size() >= k){
+                for(String reporter : set){
+                    userReports.merge(reporter, 1, Integer::sum);
                 }
             }
         }
         
         int[] answer = new int[id_list.length];
-        for(Map.Entry<String,Integer> m : mailCount.entrySet()){
-            answer[userIdx.get(m.getKey())] = m.getValue();
+        for(int i=0; i<id_list.length; i++){
+            
+            answer[i] = userReports.getOrDefault(id_list[i], 0);
         }
+        
         return answer;
     }
 }
