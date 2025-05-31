62a16a88a7acd534dd91ebf5e28e20679647c12a
TaeHyoun Kim <kth9903@gmail.com>
2025-05-31 20:40:23 +0900

    [level 2] Title: 예상 대진표, Time: 0.05 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,28 +2,39 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
-        return getRound(a < b ? a : b, a >= b ? a : b);
-    }
-    
-    private int getRound(int a, int b){
-        int r = 1;
-        while(!(b-a == 1 && a % 2 == 1)){
-            if(a %2 == 0){
-                a/=2;
-            }else{
-                a++;
-                a/=2;
-            }
-            if(b%2 == 0){
-                b/=2;
-            }else{
-                b++;
-                b/=2;
-            }
-            r++;
+        int answer = 1;
+        
+        int small,big;
+        
+        if(a>b){
+            big = a-1;
+            small = b-1; 
+        }else{
+            big = b-1;
+            small = a-1;
         }
         
+        while(true){
+            if(big - small == 1 && small % 2 == 0) break;
+            small/=2;
+            big/=2;
+            n/=2;
+            answer++;
+        }
         
-        return r;
+        return answer;
     }
 }
+
+/*
+1,2 3,4, 5,6, 7,8
+1, 2, 3, 4
+
+0, 1, 2, 3, 4, 5, 6, 7
+0, 1, 2, 3 
+0, 1
+
+
+3 -> 1 -> 0
+6 -> 3 -> 1
+*/
