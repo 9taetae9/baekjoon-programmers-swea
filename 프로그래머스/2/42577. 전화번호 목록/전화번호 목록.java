10d41ae9ac9d7d1ba39fd9aaf2bf5b105a81a501
TaeHyoun Kim <kth9903@gmail.com>
2025-08-10 00:40:33 +0900

    [level 2] Title: 전화번호 목록, Time: 367.92 ms, Memory: 97.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -4,8 +4,7 @@ class Solution {
     public boolean solution(String[] phone_book) {
         Arrays.sort(phone_book);
         for(int i=1; i<phone_book.length; i++){
-            if(phone_book[i].startsWith(phone_book[i-1])
-              || phone_book[i-1].startsWith(phone_book[i])){
+            if(phone_book[i].startsWith(phone_book[i-1])){
                 return false;
             }
         }
