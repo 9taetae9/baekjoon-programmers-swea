d0dc98edab0a6507b1b48036e8c3f1519269f2cd
TaeHyoun Kim <kth9903@gmail.com>
2025-08-12 00:08:16 +0900

    [level 3] Title: 광고 삽입, Time: 290.16 ms, Memory: 289 MB -BaekjoonHub

--- 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
@@ -1,69 +1,55 @@
-import java.util.*;
-
 class Solution {
     public String solution(String play_time, String adv_time, String[] logs) {
-        int playSec = toSec(play_time);
-        int advSec = toSec(adv_time);
-        
-        long[] changes = new long[playSec + 1];
-        
-        for(String log : logs){
+        int playSeconds = toSeconds(play_time);
+        int advSeconds = toSeconds(adv_time);
+        long[] total = new long[playSeconds + 2];  
+
+       
+        for (String log : logs) {
             String[] parts = log.split("-");
-            int start = toSec(parts[0]);
-            int end = toSec(parts[1]);
-            changes[start]++;
-            changes[end]--;
+            int start = toSeconds(parts[0]);
+            int end = toSeconds(parts[1]);
+            total[start] += 1;
+            total[end] -= 1;
         }
+
         
-        // 시청자 수(초 구간)
-        long[] viewers = new long[playSec];
-        long cur = 0;
-        for(int i=0; i < playSec; i++){
-            cur += changes[i];
-            viewers[i] = cur;
+        for (int i = 1; i <= playSeconds; i++) {
+            total[i] += total[i - 1];
         }
+
         
-        // 누적 시청자 수
-        long[] prefix = new long[playSec];
-        if(playSec > 0){
-            prefix[0] = viewers[0];
-            for(int i=1; i<playSec; i++){
-                prefix[i] = prefix[i-1] + viewers[i];
-            }
+        for (int i = 1; i <= playSeconds; i++) {
+            total[i] += total[i - 1];
         }
-        
-        int initEnd = advSec - 1;
-        long bestSum = prefix[initEnd];
-        int bestStart = 0;
-        
-        for(int s=1; s + advSec - 1 < playSec; s++){
-            int e = s + advSec - 1;
-            long sum = prefix[e] - prefix[s-1];
-            if(sum > bestSum){
-                bestSum = sum;
-                bestStart = s;
+
+        long maxView = total[advSeconds - 1];
+        int maxStart = 0;
+
+        for (int i = advSeconds; i <= playSeconds; i++) {
+            long view = total[i] - total[i - advSeconds];
+            if (view > maxView) {
+                maxView = view;
+                maxStart = i - advSeconds + 1;
             }
         }
 
-        return toTimeStr(bestStart);
+        return toTime(maxStart);
     }
-    
-    private int toSec(String time){
+
+
+    private int toSeconds(String time) {
         String[] parts = time.split(":");
-        int hour = Integer.parseInt(parts[0]);
-        int min = Integer.parseInt(parts[1]);
-        int sec = Integer.parseInt(parts[2]);
-        
-        return hour*3600 + min*60 + sec;
+        return Integer.parseInt(parts[0]) * 3600 +
+               Integer.parseInt(parts[1]) * 60 +
+               Integer.parseInt(parts[2]);
     }
-    
-    private String toTimeStr(int sec){
-        int hour = sec / 3600;
-        sec %= 3600;
-        int min = sec / 60;
-        sec %= 60;
-        return String.format("%02d:%02d:%02d", hour, min, sec);
+
+
+    private String toTime(int seconds) {
+        int h = seconds / 3600;
+        int m = (seconds % 3600) / 60;
+        int s = seconds % 60;
+        return String.format("%02d:%02d:%02d", h, m, s);
     }
-    
-    
 }
