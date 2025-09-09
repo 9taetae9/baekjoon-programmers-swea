29292805b8b67198f9c1925ae2cbc1a1e8172a0c
TaeHyoun Kim <kth9903@gmail.com>
2025-09-09 16:09:56 +0900

    [level 2] Title: 피로도, Time: 111.88 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,20 +1,35 @@
 class Solution {
-    static boolean[] visited;
-    static int max = 0;
+    private int k;
+    private int[][] dungeons;
+    private boolean[] visited;
+    
+    private int max; 
     public int solution(int k, int[][] dungeons) {
+        this.k = k;
+        this.dungeons = dungeons;
         visited = new boolean[dungeons.length];
-        dfs(k, 0, dungeons);
+        
+        dfs(0, 0);
         return max;
     }
     
-    private void dfs(int k, int count, int[][] dungeons){
-        max = Math.max(max, count);
+    private void dfs(int cnt,int step){
+        max = Math.max(max, cnt);
+        
+        if(step == dungeons.length) return;
+        
         for(int i=0; i<dungeons.length; i++){
-            if(dungeons[i][0] <= k && !visited[i]){
+            if(!visited[i] && dungeons[i][0] <= k){
                 visited[i] = true;
-                dfs(k - dungeons[i][1], count+1, dungeons);
+                k -= dungeons[i][1];
+                dfs(cnt+1, step+1);
+                k += dungeons[i][1]; 
                 visited[i] = false;
+            }else{
+                dfs(cnt, step+1);
             }
         }
     }
 }
+/**
+*/
