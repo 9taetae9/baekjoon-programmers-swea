e22ac470af342e8d9c7a9e84409e0a971b6e25d6
TaeHyoun Kim <kth9903@gmail.com>
2025-09-12 23:31:00 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 12.96 ms, Memory: 92.8 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,42 +1,54 @@
 import java.util.*;
 
 class Solution {
-    private static List<String> result = new ArrayList<>();
-    private static Map<Integer, Map<String, Integer>> countMap = new HashMap<>();
-    public String[] solution(String[] orders, int[] course) {
-        
-        
-        for(int c : course){
-            countMap.put(c, new HashMap<>());
+    //만들 수 잇는 메뉴 구성과 총 주문 수를 저장할 해시맵
+    private static HashMap<Integer, HashMap<String, Integer>> courseMap;
+    
+    public String[] solution(String[] orders, int[] course){
+        // 해시맵 초기화
+        courseMap = new HashMap<>();
+        for(int i : course){
+            courseMap.put(i, new HashMap<>());
         }
         
+        // 코스를 배열로 만들고 오름차순으로 정렬해서 가능한 모든 메뉴 구성을 구함
         for(String order : orders){
-            char[] orderArr = order.toCharArray();
-            Arrays.sort(orderArr);
-            combination(0, orderArr, "");
+            //ABCFG
+            char[] orderArray = order.toCharArray(); //A B C F G
+            Arrays.sort(orderArray);
+            combinations(0, orderArray, new StringBuilder());
         }
         
-        for(Map<String, Integer> count : countMap.values()){
-            count.values().stream()
-                .max(Comparator.comparingInt(o->o))
-                .ifPresent(cnt -> count.entrySet().stream()
-                                .filter(entry -> cnt.equals(entry.getValue()) && cnt > 1)
-                                .forEach(entry -> result.add(entry.getKey())));
+        List<String> answer = new ArrayList<>();
+        
+        for(HashMap<String, Integer> count : courseMap.values()){
+            count.values()
+                .stream()
+                .max(Comparator.comparingInt(o -> o))
+                .ifPresent(cnt -> count.entrySet()
+                          .stream()
+                          .filter(entry -> cnt.equals(entry.getValue()) && cnt > 1)
+                          .forEach(entry -> answer.add(entry.getKey())));
         }
         
-        Collections.sort(result);
-        return result.toArray(new String[0]);
-        
+        Collections.sort(answer);
+        return answer.toArray(new String[0]);
         
     }
     
-    private void combination(int idx, char[] order, String current){
-        if(countMap.containsKey(current.length())){
-            countMap.get(current.length()).merge(current, 1, Integer::sum);
+    //만들 수 있는 모든 조합을 재귀 함수를 이용해서 구현
+    public static void combinations(int idx, char[] order, StringBuilder sb){
+        if(courseMap.containsKey(sb.length())){// 해당 길이의 메뉴 구성 해시맵
+            HashMap<String, Integer> map = courseMap.get(sb.length());
+            // 해당 코스의 수를 1증가
+            String comb = sb.toString();
+            map.put(comb, map.getOrDefault(comb, 0) + 1);
         }
         
-        for(int i=idx; i<order.length; i++){
-            combination(i+1, order, current + order[i]);
+        for(int i=idx; i < order.length; i++){
+            sb.append(order[i]);
+            combinations(i+1, order, sb);
+            sb.setLength(sb.length()-1);
         }
     }
     
