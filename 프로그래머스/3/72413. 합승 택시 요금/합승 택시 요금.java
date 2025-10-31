c9a95cd264ea2a1824ec3493e0fd105f5ccd966a
TaeHyoun Kim <kth9903@gmail.com>
2025-10-31 13:51:29 +0900

    [level 3] Title: 합승 택시 요금, Time: 33.15 ms, Memory: 68.3 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,13 +1,20 @@
+import java.util.*;
+
 class Solution {
-    final int INF = 20000000;
+    class Edge{
+        int to;
+        int cost;
+        
+        Edge(int to, int cost){
+            this.to = to;
+            this.cost = cost;
+        }
+    }
+    
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        //floyd-warshall
-        int[][] dist = new int[n+1][n+1];
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n; j++){
-                if(i == j) dist[i][j] = 0;
-                else dist[i][j] = INF;
-            }
+        List<List<Edge>> graph = new ArrayList<>();
+        for(int i=0; i<=n; i++){
+            graph.add(new ArrayList<>());
         }
         
         for(int[] fare : fares){
@@ -15,25 +22,51 @@ class Solution {
             int d = fare[1];
             int f = fare[2];
             
-            dist[c][d] = f;
-            dist[d][c] = f;
+            graph.get(c).add(new Edge(d, f));
+            graph.get(d).add(new Edge(c, f));
         }
         
-        for(int k=1; k<=n; k++){
-            for(int i=1; i<=n; i++){
-                for(int j=1; j<=n; j++){
-                    if(dist[i][j] > dist[i][k] + dist[k][j]){
-                        dist[i][j] = dist[i][k] + dist[k][j];
-                    }
+        int[] distFromS = dijkstra(s, n, graph);
+        int[] distFromA = dijkstra(a, n, graph);
+        int[] distFromB = dijkstra(b, n, graph);
+        
+        int answer = Integer.MAX_VALUE;
+        for(int k = 1; k<=n; k++){
+            //s -> k + k -> a + k -> b
+            answer = Math.min(answer, distFromS[k] + distFromA[k] + distFromB[k]);
+        }
+        
+        return answer;
+    }
+    
+    private int[] dijkstra(int start, int n, List<List<Edge>> graph){
+        int[] dist = new int[n+1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[start] = 0;
+        
+        PriorityQueue<Edge> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.cost, b.cost));
+        pq.offer(new Edge(start, 0));
+        
+        while(!pq.isEmpty()){
+            Edge curr = pq.poll();
+            int currNode = curr.to;
+            int currCost= curr.cost;
+            
+            if(currCost > dist[currNode]){
+                continue;
+            }
+            
+            for(Edge next : graph.get(currNode)){
+                int nextNode = next.to;
+                int nextCost = currCost + next.cost;
+                
+                if(nextCost < dist[nextNode]){
+                    dist[nextNode] = nextCost;
+                    pq.offer(new Edge(nextNode, nextCost));
                 }
             }
         }
         
-        int answer = Integer.MAX_VALUE;
-        for(int k=1; k<=n; k++){
-            answer = Math.min(answer, dist[s][k] + dist[k][a] + dist[k][b]);
-        }
-        
-        return answer;
+        return dist;
     }
 }
